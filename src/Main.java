9b91d7335546badbd4b6cdcb159e0f93b86c290c
Jarbas Santos <jarbells@outlook.com>
2024-05-19 10:02:15 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int pass = sc.nextInt();
+		
+		while (pass != 2002) {
+			System.out.println("Senha Invalida");
+			pass = sc.nextInt();
+		}
+		
+		System.out.println("Acesso Permitido");
+		
+		sc.close();
+	}
+}
